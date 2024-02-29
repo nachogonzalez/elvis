35bad3e71a56daccefb27d768543e56d190b1f69
slug <nacho.gonzalez@gmail.com>
2024-02-29 17:04:06 +0100

    Comments and logs clean-up

--- dbutils.py
+++ dbutils.py
@@ -46,29 +46,29 @@ def create_empty_excel_database(file_name):
         sheet = workbook.active
         # We save the file
         workbook.save(file_name)
-        logger.debug("Hemos creado el fichero: " + file_name)
         # We change the name of the sheets and create the headers
         if(file_name == "emails.xlsx"):
-            logger.debug("Creamos la estructura de la excel de emails")
-
+            # We create the structure of the emails excel file
+            # Tab Dashboard
             new_tab_name = 'Dashboard'
             cell_data = {'A1': 'TotalNumber', 'B1': 'DomainsNumber'}
             create_and_write_to_excel(file_name, new_tab_name, cell_data)
-
+            # Tab emails
             new_tab_name = 'Emails'
             cell_data = {'A1': 'Email', 'B1': 'Domain', 'C1': 'DateAdded'}
             create_and_write_to_excel(file_name, new_tab_name, cell_data)
- 
+            # TODO: Delete "Sheet" tab of the emails excel file
         else:
-            logger.debug("Creamos la estructura de la excel de links")
-            
+            # We create the structure of the links excel file
+            # Tab Dashboard
             new_tab_name = 'Dashboard'
             cell_data = {'A1': 'TotalNumber'}
             create_and_write_to_excel(file_name, new_tab_name, cell_data)        
-
+            # Tab Links
             new_tab_name = 'Links'
             cell_data = {'A1': 'Link', 'B1': 'DateAdded', 'C1': 'Processed', 'D1': 'DateProcessed'}
             create_and_write_to_excel(file_name, new_tab_name, cell_data)
+            # TODO: Delete "Sheet" tab of the links excel file
 
         logger.info("Excel file created successfuly: " + file_name)
 
@@ -100,15 +100,16 @@ def write_to_excel(file_path, sheet_name, cell_address, text_to_write):
     - text_to_write (str): The text to write to the cell.
     """
     logger.info("Start write_to_excel module")
+    logger.info("Parameter file_path: " + file_path)
+    logger.info("Parameter sheet_name: " + sheet_name)
+    logger.info("Parameter cell_address: " + cell_address)
+    logger.info("Parameter text_to_write: " + text_to_write)
     # Load the workbook
     workbook = openpyxl.load_workbook(file_path)
-
     # Select the sheet
     sheet = workbook[sheet_name]
-
     # Write text to the specified cell
     sheet[cell_address] = text_to_write
-
     # Save the changes
     workbook.save(file_path)
 
@@ -121,26 +122,23 @@ def rename_excel_sheet(file_path, current_sheet_name, new_sheet_name):
     - current_sheet_name (str): The current name of the sheet to be renamed.
     - new_sheet_name (str): The new name for the sheet.
     """
+    logger.info("Start rename_excel_sheet module")
+    logger.info("Parameter file_path: " + file_path)
+    logger.info("Parameter current_sheet_name: " + current_sheet_name)
+    logger.info("Parameter new_sheet_name: " + new_sheet_name)
     # Load the workbook
     workbook = openpyxl.load_workbook(file_path)
-
     # Check if the sheet exists
     if current_sheet_name in workbook.sheetnames:
         # Get the sheet
         sheet = workbook[current_sheet_name]
-
         # Rename the sheet
         sheet.title = new_sheet_name
-
         # Save the changes
         workbook.save(file_path)
         logger.info("The sheet '{current_sheet_name}' has been renamed to '{new_sheet_name}'.")
     else:
         logger.info("The sheet '{current_sheet_name}' does not exist in the Excel file.")
-
-import openpyxl
-
-import openpyxl
 
 def create_and_write_to_excel(file_path, new_tab_name, cell_data):
     """
@@ -152,26 +150,17 @@ def create_and_write_to_excel(file_path, new_tab_name, cell_data):
     - cell_data (dict): A dictionary where keys are cell addresses (e.g., 'A1', 'B2') 
                        and values are the text to write to each cell.
     """
+    logger.info("Start create_and_write_to_excel module")
+    logger.info("Parameter file_path: " + file_path)
+    logger.info("Parameter new_tab_name: " + new_tab_name)
+    logger.info("Parameter cell_data: " + cell_data)    
     # Load the workbook
     workbook = openpyxl.load_workbook(file_path)
-
     # Create a new worksheet
     new_tab = workbook.create_sheet(title=new_tab_name)
-
     # Write text to multiple cells in the new worksheet
     for cell_address, text_to_write in cell_data.items():
         new_tab[cell_address] = text_to_write
-
     # Save the changes
     workbook.save(file_path)
-
-    print(f"The new tab '{new_tab_name}' has been created, and text has been written to the specified cells.")
-
-
-
-
-
-
-
-
-
+    logger.info("The new tab '{new_tab_name}' has been created, and text has been written to the specified cells.")
